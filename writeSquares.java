75e15c60263bab603b7d53804846df2ef483ef3f
Seojin-Won <docojay@gmail.com>
2020-11-18 02:57:35 -0800

    끝까지하면된다!

---
+++ writeSquares.java
@@ -0,0 +1,36 @@
+/*
+Write a method writeSquares that accepts an integer parameter n and 
+prints the first n squares separated by commas, 
+with the odd squares in descending order 
+followed by the even squares in ascending order. 
+The following table shows several calls to the method and their expected output:
+
+Call	Valued Returned
+writeSquares(5);	25, 9, 1, 4, 16
+writeSquares(1);	1
+writeSquares(8);	49, 25, 9, 1, 4, 16, 36, 64
+Your method should throw an IllegalArgumentException 
+if passed a value less than 1. Note that the output does not advance onto the next line.
+*/
+
+public void writeSquares(int n){
+    if(n < 1){
+        throw new IllegalArgumentException();
+    }
+    
+    if(n == 1){
+        System.out.print(1);
+    }
+    else{ //among rest
+        if(n%2 == 1){ //odd
+            System.out.print((int)Math.pow(n, 2) + ", ");
+            writeSquares(n-2);
+            System.out.print(", " + (int)Math.pow(n-1, 2));
+        }
+        else{ //even
+            writeSquares(n-1);
+            System.out.print(", " + (int)Math.pow(n, 2));
+        }
+    }
+    
+}
